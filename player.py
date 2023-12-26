d7eb628fc01b3762b885a97b4d12cea488f6a01b
Patrick Grimard <patrick.grimard@xtl.com>
2023-12-26 16:12:54 -0500

    Create Stance enum class

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import logging
 
 import pygame
@@ -10,19 +11,25 @@ def handle_key(key):
         return wrapper
     return decorator
 
+class Stance(Enum):
+    UP: str = "UP"
+    DOWN: str = "DOWN"
+    LEFT: str = "LEFT"
+    RIGHT: str = "RIGHT"
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x: int, pos_y: int):
         super().__init__()
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.speed = 7
-        self.stance = "up"
+        self.stance = Stance.UP
         self.rect = pygame.Rect(pos_x, pos_y, 50, 50)
 
     @handle_key(pygame.K_UP)
     def move_up(self):
         logging.debug("Player facing up")
-        self.stance = "up"
+        self.stance = Stance.UP
         
         if self.pos_y > 0:
             self.pos_y -= self.speed
@@ -31,7 +38,7 @@ class Player(pygame.sprite.Sprite):
     @handle_key(pygame.K_DOWN)
     def move_down(self):
         logging.debug("Player facing down")
-        self.stance = "down"
+        self.stance = Stance.DOWN
 
         if self.pos_y < 600:
             self.pos_y += self.speed
@@ -40,7 +47,7 @@ class Player(pygame.sprite.Sprite):
     @handle_key(pygame.K_LEFT)
     def move_left(self):
         logging.debug("Player facing left")
-        self.stance = "left"
+        self.stance = Stance.LEFT
 
         if self.pos_x > 0:
             self.pos_x -= self.speed
@@ -49,7 +56,7 @@ class Player(pygame.sprite.Sprite):
     @handle_key(pygame.K_RIGHT)
     def move_right(self):
         logging.debug("Player facing right")
-        self.stance = "right"
+        self.stance = Stance.RIGHT
 
         if self.pos_x < 800:
             self.pos_x += self.speed
@@ -57,13 +64,13 @@ class Player(pygame.sprite.Sprite):
 
     @handle_key(pygame.K_SPACE)
     def handle_space_key(self):
-        if self.stance == "up":
+        if self.stance == Stance.UP:
             logging.debug("Player is shooting up")
-        elif self.stance == "down":
+        elif self.stance == Stance.DOWN:
             logging.debug("Player is shooting down")
-        if self.stance == "left":
+        if self.stance == Stance.LEFT:
             logging.debug("Player is shooting left")
-        if self.stance == "right":
+        if self.stance == Stance.RIGHT:
             logging.debug("Player is shooting right")
 
     def handle_events(self):
