af92f00eb5f3dc3ecc19e416f948f3d8dd5ece43
Patrick Grimard <patrick.grimard@xtl.com>
2023-12-29 07:25:38 -0500

    Change Enemy to a sprite

--- enemy.py
+++ enemy.py
@@ -1,37 +1,22 @@
 import random
-from dataclasses import dataclass
 
 import pygame
 
-from board import Board
 
-
-@dataclass
-class Enemy():
-    size: int
-    x: int
-    y: int
-
-    @property
-    def rect(self):
-        return pygame.Rect(self.x, self.y, self.size, self.size)
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, board_width: int, board_height: int):
+        super().__init__()
+        self.image = pygame.image.load("assets/coin.png")
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (
+            random.randint(0, board_width - self.rect.width),
+            random.randint(0, board_height - self.rect.height),
+        )
     
     @property
-    def xp(self):
+    def xp_value(self):
         return random.randint(1, 10)
 
 
-def spawn_enemies(board: Board, count: int = 1) -> list[Enemy]:
-    enemies = []
-
-    for i in range(count):
-        enemy = spawn_random_enemy(board.width, board.height)
-        enemies.append(enemy)
-
-    return enemies
-
-def spawn_random_enemy(board_width: int, board_height: int) -> Enemy:
-    size = 25 #random.randint(10, 25)
-    x = random.randint(0, board_width - size)
-    y = random.randint(0, board_height - size)
-    return Enemy(size=size, x=x, y=y)
+def spawn_enemies(board_width: int, board_height: int, count: int = 1) -> list[Enemy]:
+    return [Enemy(board_width, board_height) for i in range(count)]
